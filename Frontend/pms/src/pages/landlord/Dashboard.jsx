3b028e9bc2b50dc41bc39554ddf5366b136ee470
Omar Hemed <omarhemed800@gmail.com>
2024-03-08 18:03:11 +0300

    Included the tenant table in the landlord/property manager dashboard

--- Frontend/pms/src/pages/landlord/Dashboard.jsx
+++ Frontend/pms/src/pages/landlord/Dashboard.jsx
@@ -1,7 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import StatsCard from "../../components/StatsCard";
+import TenantTable from "./partials/TenantTable";
 
 export default function Dashboard () {
+    const tenants = [
+        { name: 'John Doe', email: 'john@example.com', phone: '123-456-7890', property: 'Uni101', 
+        propertyType: 'Commercial Building', rentAmount: '20,000' , status: 'Unpaid'
+        },
+        { name: 'Jane Smith', email: 'jane@example.com', phone: '987-654-3210', status: 'paid', 
+        property: 'Unitcom102', propertyType: 'Commercial Building', rentAmount: '20,000' ,  
+        },
+        // Add more tenant objects as needed
+      ];
     return (
         <>
             <section className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
@@ -21,6 +31,9 @@ export default function Dashboard () {
             <section className="bg-white rounded-lg shadow-md h-96 mt-4">
                 <div className="px-6 py-5 font-semibold border-b border-gray-100">
                     Tenants
+                </div>
+                <div className="overflow-x-auto">
+                    <TenantTable tenants={tenants} />
                 </div>
             </section>
         </>
