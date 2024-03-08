664505c5470a477adae9d78f54dae800a1542978
Omar Hemed <omarhemed800@gmail.com>
2024-03-08 18:33:58 +0300

    Modify the tenants section header in Landlord Dashboard with filter dropdowon button and a button to add tenants

--- Frontend/pms/src/pages/landlord/Dashboard.jsx
+++ Frontend/pms/src/pages/landlord/Dashboard.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import StatsCard from "../../components/StatsCard";
 import TenantTable from "./partials/TenantTable";
+import FilterDropdown from "../../components/Filter";
+import PrimaryButton from "../../components/PrimaryButton";
 
 export default function Dashboard () {
+    const [filterValue, setFilterValue] = useState('');
+
     const tenants = [
         { name: 'John Doe', email: 'john@example.com', phone: '123-456-7890', property: 'Uni101', 
         propertyType: 'Commercial Building', rentAmount: '20,000' , status: 'Unpaid'
@@ -10,8 +15,19 @@ export default function Dashboard () {
         { name: 'Jane Smith', email: 'jane@example.com', phone: '987-654-3210', status: 'paid', 
         property: 'Unitcom102', propertyType: 'Commercial Building', rentAmount: '20,000' ,  
         },
-        // Add more tenant objects as needed
-      ];
+    ];
+  
+    const filteredTenants = tenants.filter(tenant => {
+    return tenant.name.toLowerCase().includes(filterValue.toLowerCase()) ||
+            tenant.email.toLowerCase().includes(filterValue.toLowerCase());
+    });
+    const filterItems = [
+        { id: "1", name: "Name"},
+        { id: "2", name: "Property"},
+        { id: "3", name: "Status"},
+        { id: "4", name: "Property type"},
+        { id: "5", name: "Rent amount"},
+    ];
     return (
         <>
             <section className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
@@ -29,11 +45,20 @@ export default function Dashboard () {
                 </StatsCard>
             </section>
             <section className="bg-white rounded-lg shadow-md h-96 mt-4">
-                <div className="px-6 py-5 font-semibold border-b border-gray-100">
-                    Tenants
+                <div className="flex justify-between px-6 py-5 font-semibold border-b border-gray-100">
+                    <div>
+                        Tenants
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        <FilterDropdown items={filterItems} onChange={setFilterValue} />
+                        <PrimaryButton >
+                            <FontAwesomeIcon icon="plus" className="mr-2" />
+                            Add Tenant
+                        </PrimaryButton>
+                    </div>
                 </div>
                 <div className="overflow-x-auto">
-                    <TenantTable tenants={tenants} />
+                    <TenantTable tenants={filteredTenants} />
                 </div>
             </section>
         </>
